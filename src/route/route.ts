766a7c3ad1dad1a898d0c85d90972ecb89e35383
Fadhli Firdausi <fadsayma@gmail.com>
2025-06-12 16:00:08 +0700

    fix index lagi

--- src/route/route.ts
+++ src/route/route.ts
@@ -3,12 +3,12 @@ import express from 'express'
 import RecipeController from '../controller/recipe_controller';
 
 const router = express.Router()
-router.get(".recipes", )
+
 
 router.get("/recipes", RecipeController.index)
-router.get("/recipes/:id", RecipeController.show)
-router.post("/recipes", upload.single("image"), RecipeController.store);
-router.put("/recipes/:id", upload.single("image"), RecipeController.update)
-router.delete("/recipes/:id", RecipeController.destory);
+router.get("/recipe/:id", RecipeController.show)
+router.post("/recipe", upload.single("image"), RecipeController.store);
+router.put("/recipe/:id", upload.single("image"), RecipeController.update)
+router.delete("/recipe/:id", RecipeController.destory);
 
 export default router
