b0c06207f4b5a1069eaa5976dc14d3ae64b93855
Fadhli Firdausi <fadsayma@gmail.com>
2025-06-12 20:42:58 +0700

    tdhf

--- src/middleware/upload.ts
+++ src/middleware/upload.ts
@@ -9,8 +9,8 @@ const storage = multer.diskStorage({
   destination: (req, file, cb) => cb(null, uploadDir),
   filename: (req, file, cb) => {
     const ext = path.extname(file.originalname);
-    const name = `${Date.now()}-${ext}`;
-    //const name = `${Date.now()}-${file.originalname}`;
+    // const name = `${Date.now()}-${ext}`;
+    const name = `${Date.now()}-${file.originalname}`;
     cb(null, name);
   },
 });
